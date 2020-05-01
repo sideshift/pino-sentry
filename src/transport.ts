49f02d4a913a9bc85a615d453030bffbfbdb750d
Andreas Brekken <andreas@brekken.com>
2020-05-01 16:00:47 +0700

    Submit data as string to prevent truncating

--- src/transport.ts
+++ src/transport.ts
@@ -1,4 +1,4 @@
-import stream  from 'stream';
+import stream from 'stream';
 import split from 'split2';
 import pump from 'pump';
 import through from 'through2';
@@ -9,14 +9,14 @@ class ExtendedError extends Error {
   public constructor(info: any) {
     super(info.message);
 
-    this.name = "Error";
+    this.name = 'Error';
     this.stack = info.stack || null;
   }
 }
 
 function defaults<T>(target: Partial<T>, source: Partial<T>): Partial<T> {
   const ret: Partial<T> = { ...target };
-  for (const key of (Object.keys(source) as (keyof T)[])) {
+  for (const key of Object.keys(source) as (keyof T)[]) {
     if (target[key] === undefined) {
       ret[key] = source[key];
     }
@@ -26,12 +26,12 @@ function defaults<T>(target: Partial<T>, source: Partial<T>): Partial<T> {
 
 export class PinoSentryTransport {
   private SEVERITIES_MAP = {
-    10: Sentry.Severity.Debug,   // pino: trace
-    20: Sentry.Severity.Debug,   // pino: debug
-    30: Sentry.Severity.Info,    // pino: info
+    10: Sentry.Severity.Debug, // pino: trace
+    20: Sentry.Severity.Debug, // pino: debug
+    30: Sentry.Severity.Info, // pino: info
     40: Sentry.Severity.Warning, // pino: warn
-    50: Sentry.Severity.Error,   // pino: error
-    60: Sentry.Severity.Fatal,   // pino: fatal
+    50: Sentry.Severity.Error, // pino: error
+    60: Sentry.Severity.Fatal, // pino: fatal
     // Support for useLevelLabels
     // https://github.com/pinojs/pino/blob/master/docs/api.md#uselevellabels-boolean
     trace: Sentry.Severity.Debug,
@@ -56,8 +56,7 @@ export class PinoSentryTransport {
 
   public parse(line: any) {
     const chunk = JSON.parse(line);
-    const cb = () => {
-    };
+    const cb = () => {};
 
     this.prepareAndGo(chunk, cb);
   }
@@ -90,13 +89,13 @@ export class PinoSentryTransport {
     const message = chunk.msg;
     const stack = chunk.stack || '';
 
-    Sentry.configureScope(scope => {
+    Sentry.configureScope((scope) => {
       if (this.isObject(tags)) {
-        Object.keys(tags).forEach(tag => scope.setExtra(tag, tags[tag]));
+        Object.keys(tags).forEach((tag) => scope.setExtra(tag, tags[tag]));
       }
 
       if (this.isObject(data)) {
-        scope.setExtra('data', data);
+        scope.setExtra('data', JSON.stringify(data, null, 2));
       }
     });
 
@@ -135,13 +134,13 @@ export class PinoSentryTransport {
 
   private isObject(obj: any): boolean {
     const type = typeof obj;
-    return type === 'function' || type === 'object' && !!obj;
+    return type === 'function' || (type === 'object' && !!obj);
   }
 
   private shouldLogException(level: Sentry.Severity): boolean {
     return level === Sentry.Severity.Fatal || level === Sentry.Severity.Error;
   }
-};
+}
 
 export function createWriteStreamAsync(options: Sentry.NodeOptions = {}): PromiseLike<stream.Transform> {
   if (!options.dsn && !process.env.SENTRY_DSN) {
@@ -152,17 +151,22 @@ export function createWriteStreamAsync(options: Sentry.NodeOptions = {}): Promis
   const sentryTransformer = transport.transformer();
 
   const pumpAsync = pify(pump);
-  return pumpAsync(process.stdin, split((line) => {
-    try {
-      return JSON.parse(line);
-    } catch (e) {
-      throw Error('logs should be in json format');
-    }
-  }), sentryTransformer);
-};
+  return pumpAsync(
+    process.stdin,
+    split((line) => {
+      try {
+        return JSON.parse(line);
+      } catch (e) {
+        throw Error('logs should be in json format');
+      }
+    }),
+    sentryTransformer
+  );
+}
 
-
-export function createWriteStream(options: Sentry.NodeOptions = {}): stream.Transform & { transport: PinoSentryTransport } {
+export function createWriteStream(
+  options: Sentry.NodeOptions = {}
+): stream.Transform & { transport: PinoSentryTransport } {
   if (!options.dsn && !process.env.SENTRY_DSN) {
     throw Error('Sentry DSN missing');
   }
@@ -171,4 +175,4 @@ export function createWriteStream(options: Sentry.NodeOptions = {}): stream.Tran
   const sentryParse = transport.parse.bind(transport);
 
   return Object.assign(split(sentryParse), { transport });
-};
+}
